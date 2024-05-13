8690665bf9d6a7238d0fe2a3883fe6ba9dbe9be9
Tarun Hasija <tarunhasija999@gmail.com>
2024-05-13 14:08:58 +0530

    new commit

--- lib/constant.dart
+++ lib/constant.dart
@@ -5,4 +5,4 @@ import 'package:flutter/material.dart';
 double deviceHeight = MediaQuery.of(context as BuildContext).size.height;
 double deviceWidth = MediaQuery.of(context as BuildContext).size.width;
 
-Color primary = C
+Color primary = Colors.grey;
