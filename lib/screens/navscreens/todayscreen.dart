040ca1c1c90ee15424a53286d2b336a5d8353da0
Tarun Hasija <tarunhasija999@gmail.com>
2024-05-29 05:11:08 +0530

    new commit

--- lib/screens/navscreens/todayscreen.dart
+++ lib/screens/navscreens/todayscreen.dart
@@ -17,15 +17,51 @@ class _TodayScreenState extends State<TodayScreen> {
       child: Column(
         children: [
           Container(
-            margin: const EdgeInsets.only(top: 32),
             alignment: Alignment.centerLeft,
+            margin: const EdgeInsets.only(top: 32),
             child: Text("Welcome",style: TextStyle(
               color: Colors.black54,
               letterSpacing: 2,
-              fontSize: deviceWidth(context)/26,
+              fontSize: deviceWidth(context)*.05
               
             ),),
-          )
+          ),
+          Container(
+            alignment: Alignment.centerLeft,
+            child: Text("Employee",style: TextStyle(
+              letterSpacing: 2,
+              fontSize: deviceWidth(context)*.06
+              
+            ),),
+          ),
+          Container(
+            alignment: Alignment.centerLeft,
+            margin: const EdgeInsets.only(top: 32),
+            child: Text("Today's Status",style: TextStyle(
+              letterSpacing: 2,
+              fontSize: deviceWidth(context)*.05
+              
+            ),),
+          ),
+          Container(
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black26,
+                  blurRadius: 10,
+                  offset: Offset(2, 2)
+                )
+              ]
+            ),
+            alignment: Alignment.centerLeft,
+            margin: const EdgeInsets.only(top: 32),
+            child: Text("Today's Status",style: TextStyle(
+              letterSpacing: 2,
+              fontSize: deviceWidth(context)*.05
+              
+            ),),
+          ),
         ],
       ),
     ));
