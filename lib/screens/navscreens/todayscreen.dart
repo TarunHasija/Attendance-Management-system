470d060d3835037e3a673ae0c7b137eaa4cbea8f
Tarun Hasija <tarunhasija999@gmail.com>
2024-07-08 13:47:05 +0530

    final commit

--- lib/screens/navscreens/todayscreen.dart
+++ lib/screens/navscreens/todayscreen.dart
@@ -4,7 +4,6 @@ import 'package:ams/constant.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:intl/intl.dart';
 import 'package:slide_to_act/slide_to_act.dart';
